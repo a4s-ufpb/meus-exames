0d188a19058ea4aa14bfd197f25899d10c30c12c
João Miguel Santos <158611854+ricoRecruta@users.noreply.github.com>
2026-02-27 11:01:16 -0300

    add pdf upload endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from fastapi.exceptions import HTTPException
 
 app = FastAPI(title="Medical API")
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.post("/upload")
+def upload(file: UploadFile):
+    data = file.file.read()
+    return {"filename": file.filename}
